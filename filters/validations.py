6b8380109e27329a6bdd312615421317a73fd87e
Manjit Kumar <manjitkumar@users.noreply.github.com>
2016-06-27 10:45:16 +0530

    update validation for CSV of integers (#9)
    * update validation for CSV of integers
    
    * Update validations.py

--- filters/validations.py
+++ filters/validations.py
@@ -96,8 +96,8 @@ def DatetimeWithTZ(msg=None):
 def CSVofIntegers(msg=None):
     '''
     Checks whether a value is list of integers.
-    Returns list of integers or just an integer
-    if there is only one element in list.
+    Returns list of integers or just one integer in 
+    list if there is only one element in given CSV string.
     '''
     def fn(value):
         try:
@@ -112,7 +112,7 @@ def CSVofIntegers(msg=None):
                     )
                     return value
                 else:
-                    return int(value)
+                    return [int(value)]
         except ValueError:
             raise Invalid(
                 '<{0}> is not a valid csv of integers'.format(value)
